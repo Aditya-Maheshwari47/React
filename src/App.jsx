ff9c15fabd0cc2d5cfef0a594354b2b68d67ec39
Aditya Maheshwari <aditya.maheshwari@einfochips.com>
2026-02-09 17:28:49 +0530

    Add reusable components using props(9/2/2026)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import reactImg from "./assets/react-core-concepts.png"
+import comoponentsImg from "./assets/components.png"
+
+import {CORE_CONCEPTS} from './data.js'
+
 const reactDescriptions = ["Fundamental", "Description", "Core"];
 
 // get random value [0,max]
@@ -19,14 +23,43 @@ function Header() {
     </header>
   );
 }
-
+//props
+function CoreConcepts(props){
+  return(
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      
+    </li>
+  );
+}
 function App() {
   return (
     <div>
       {/* component nesting */}
       <Header/>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts
+            title = "Components"
+            description = "The core UI The core UI building block - compose the user interface by combining multiple components." 
+            image = {comoponentsImg}
+          
+          />
+          <CoreConcepts
+            title = {CORE_CONCEPTS[1].title}
+            description = {CORE_CONCEPTS[1].description}
+            image = {CORE_CONCEPTS[1].image}
+
+          />  
+          {/* alternate way using spread operator */}
+          <CoreConcepts {...CORE_CONCEPTS[2]}/>   
+          <CoreConcepts {...CORE_CONCEPTS[3]}/> 
+        </ul>
+        </section>
       </main>
     </div>
   );
