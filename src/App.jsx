3229a87da562101096b2ec98a92e4d938bdab703
Aditya Maheshwari <aditya.maheshwari@einfochips.com>
2026-02-09 18:07:56 +0530

    Reorganize component structure into separate files(9/2/2026)

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
-import reactImg from "./assets/react-core-concepts.png"
-import comoponentsImg from "./assets/components.png"
-
 import {CORE_CONCEPTS} from './data.js'
+import Header from './components/Header.jsx'
+import CoreConcepts from "./components/CoreConcept.jsx";
 
-const reactDescriptions = ["Fundamental", "Description", "Core"];
-
-// get random value [0,max]
-function genRandomInt(max){
-  return Math.floor (Math.random() * (max+1));
-}
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" /> 
-      <h1>React Essentials</h1>
-      <p>
-        {/* dynamic values */}
-        {description} React concepts you will need for almost any app you are         
-        going to build!
-      </p>
-    </header>
-  );
-}
-//props
-function CoreConcepts(props){
-  return(
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-      
-    </li>
-  );
-}
 function App() {
   return (
     <div>
@@ -44,18 +12,12 @@ function App() {
         <h2>Core Concepts</h2>
         <ul>
           <CoreConcepts
-            title = "Components"
-            description = "The core UI The core UI building block - compose the user interface by combining multiple components." 
-            image = {comoponentsImg}
-          
+            title = {CORE_CONCEPTS[0].title}
+            description = {CORE_CONCEPTS[0].description}
+            image = {CORE_CONCEPTS[0].image}
           />
-          <CoreConcepts
-            title = {CORE_CONCEPTS[1].title}
-            description = {CORE_CONCEPTS[1].description}
-            image = {CORE_CONCEPTS[1].image}
-
-          />  
           {/* alternate way using spread operator */}
+          <CoreConcepts {...CORE_CONCEPTS[1]}/>  
           <CoreConcepts {...CORE_CONCEPTS[2]}/>   
           <CoreConcepts {...CORE_CONCEPTS[3]}/> 
         </ul>
