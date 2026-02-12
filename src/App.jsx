d040d4db0803cde0c5b660ffa42a51970896ea67
Aditya Maheshwari <aditya.maheshwari@einfochips.com>
2026-02-12 14:21:22 +0530

    Add conditional rendering with dynamic and CSS-based styling(12-2-2026)

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { EXAMPLES } from './data.js';
 import { useState } from 'react';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components'); //intialize value with 'component'
+  const [selectedTopic, setSelectedTopic] = useState();
   
   function handleSelect(selectedButton){
     //selectedButton => 'components', 'Props', 'JSX', 'State'
@@ -38,20 +38,30 @@ function App() {
                if we use --> <TabButton onSelect = {handleSelect}>State</TabButton>
             */}
             {/* passing custom arguments using () => */}
-            <TabButton onSelect = {() =>handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect = {() =>handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect = {() =>handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect = {() =>handleSelect('state')}>State</TabButton>
+            <TabButton 
+              isSelected={selectedTopic ==='components'}
+              onSelect = {() =>handleSelect('components')}>Components</TabButton>
+            <TabButton 
+              isSelected={selectedTopic ==='props'}
+              onSelect = {() =>handleSelect('props')}>Props</TabButton>
+            <TabButton
+              isSelected={selectedTopic ==='jsx'}
+              onSelect = {() =>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton
+              isSelected={selectedTopic ==='state'}
+              onSelect = {() =>handleSelect('state')}>State</TabButton>
           </menu>
-          {/* Event -> rightarrow -> Setter Function -> State Change -> Re-render -> UI Update */}
-          <div id = "tab-content">  
+          {/* Event -> Setter Function -> State Change -> Re-render -> UI Update */}
+          
+          {/* content conditionality : will render "paragraph" or selectedTopic by selecting*/}
+          {!selectedTopic ? <p>Please select a topic</p> : <div id = "tab-content">  
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
               <code>{EXAMPLES[selectedTopic].code}</code>
             </pre>
-          </div>
-    
+          </div>}
+          
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,9 @@
 
-export default function TabButton({children, onSelect}){
+export default function TabButton({children, onSelect, isSelected}){
     return (
         // 'wire up' the custom prop(onSelect) to a real browser event (onClick).
-        <li onClick={onSelect} ><button>{children}</button></li>
+        <li>
+            <button className={isSelected ? "active" : undefined} onClick={onSelect} >{children}</button>
+        </li>
     );
 }
