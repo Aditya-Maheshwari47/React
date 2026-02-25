e70cec27bfa10d4c08db017daa263a055d7cc4b1
Aditya Maheshwari <aditya.maheshwari@einfochips.com>
2026-02-25 17:46:56 +0530

    feat: Implement useImperativeHandle for component refs and create Portals for Modal - Added useImperativeHandle to forward refs to custom components for better control over their methods and state. - Refactored Modal component to use React Portals, ensuring proper rendering of the modal outside the normal DOM hierarchy. (25-02-26)

--- Refs And Portals/src/components/ResultModal.jsx
+++ Refs And Portals/src/components/ResultModal.jsx
@@ -1,16 +1,40 @@
-export default function ResultModal({result, targetTime}){
-    return ( 
-        <dialog className="result-modal" open>
-            <h2>You {result}</h2>
+import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal} from "react-dom"
+
+const ResultModal = forwardRef(function ResultModal(
+    {targetTime, remainingTime, onReset},
+         ref)
+    {
+    const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) *100);
+
+    useImperativeHandle(ref, ()=>{
+        return {
+            open(){
+                dialog.current.showModal();
+            }
+        };
+    });
+    //using createPortal the Modal is in model div instead of content div
+    return createPortal( 
+        <dialog  ref = {dialog} className="result-modal">
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2> }
             <p>
                 The target time was <strong> {targetTime} seconds.</strong> 
             </p>
             <p>
-                You stopped the timer with <strong>X seconds lefts.</strong> 
+                You stopped the timer with{' '}<strong>{formattedRemainingTime} seconds lefts.</strong> 
             </p>
-            <form method = "dialog">
+            <form method = "dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
+        , document.getElementById('modal')
     )
-}
+});
+
+export default ResultModal;
