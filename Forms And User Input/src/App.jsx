b9e8f174a192f65f5b2fe5a90bb7190228c82d86
Aditya Maheshwari <aditya.maheshwari@einfochips.com>
2026-03-12 18:07:26 +0530

    Add React form handling with state, refs, FormData, and lost focus validation (12-3-2026)

---
+++ Forms And User Input/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './components/Header.jsx';
+import Login from './components/Login.jsx';
+import StateLogin from './components/StateLogin.jsx';
+import Signup from './components/Signup.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        {/* <Signup /> */}
+        <StateLogin/>
+      </main>
+    </>
+  );
+}
+
+export default App;
